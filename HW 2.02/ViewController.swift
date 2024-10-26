101376710af388441e8eae27fae276850d166fa4
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-10-27 02:55:26 +0500

    create logic for lights

--- HW 2.02/ViewController.swift
+++ HW 2.02/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redLightOutlet: UIView!
+    @IBOutlet var yellowLightOutlet: UIView!
+    @IBOutlet var greenLightOutlet: UIView!
+    @IBOutlet var startButtonOutlet: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOff = 0.3
+    private let lightIsOn  = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightOutlet.layer.cornerRadius    = 50
+        yellowLightOutlet.layer.cornerRadius = 50
+        greenLightOutlet.layer.cornerRadius  = 50
+        startButtonOutlet.layer.cornerRadius = 10
+        
+        redLightOutlet.alpha    = lightIsOff
+        yellowLightOutlet.alpha = lightIsOff
+        greenLightOutlet.alpha  = lightIsOff
     }
-
-
+    
+    
+    @IBAction func StartButtonAction() {
+        startButtonOutlet.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLightOutlet.alpha   = CGFloat(lightIsOn)
+            greenLightOutlet.alpha = lightIsOff
+            
+            currentLight = CurrentLight.yellow
+        case .yellow:
+            yellowLightOutlet.alpha = CGFloat(lightIsOn)
+            redLightOutlet.alpha    = lightIsOff
+            
+            currentLight = CurrentLight.green
+        case .green:
+            greenLightOutlet.alpha  = CGFloat(lightIsOn)
+            yellowLightOutlet.alpha = lightIsOff
+            
+            currentLight = CurrentLight.red
+        }
+    }
 }
 
